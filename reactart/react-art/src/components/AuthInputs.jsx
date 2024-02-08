461e26f4569da1db2f663a6a6b7326a778934057
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-08 17:41:53 +0530

    tailwind-css

--- reactart/react-art/src/components/AuthInputs.jsx
+++ reactart/react-art/src/components/AuthInputs.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-import {styled} from 'styled-components'; // To use installed file style-components
+// import {styled} from 'styled-components'; // To use installed file style-components
 import Button from './Button.jsx';
 import Input from './input.jsx';
 
 //div ni css ne inline method ma use karva but easily.
  
-const Control = styled.div`
-display: flex;
-flex-direction: column;
-gap: 0.5rem;
-margin-bottom: 1.5rem;
-`
+// const Control = styled.div`
+// display: flex;
+// flex-direction: column;
+// gap: 0.5rem;
+// margin-bottom: 1.5rem;
+// `
 
 
 
@@ -35,9 +35,9 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <Control>
-        
+    <div id="auth-inputs" className='w-full max-w-sm mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
+        {/* <Controls> */}
 
           <Input
             type="email"
@@ -57,9 +57,9 @@ export default function AuthInputs() {
             }
           />
       
-      </Control>
-      <div className="actions">
-        <Button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4 ">
+        <Button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </Button>
         <button className='button' onClick={handleLogin}>Sign In</button>
--- reactart/react-art/tailwind.config.js
+++ reactart/react-art/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // to add extra fonts from google fonts.
+      fontFamily:{
+        title: ['"Pacifico"', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
